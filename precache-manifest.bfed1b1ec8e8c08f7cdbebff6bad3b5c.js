b926e9b934be79154d43ebe3645947e4199d4d44
elir-mahad <mmahad280@gmail.com>
2020-11-15 22:25:58 -0500

    Updates

---
+++ precache-manifest.bfed1b1ec8e8c08f7cdbebff6bad3b5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ad4b0836043d0d3db48fbecd4ed5600",
+    "url": "/searchsquid/index.html"
+  },
+  {
+    "revision": "89a85f79f77a3fafaa5c",
+    "url": "/searchsquid/static/css/main.78cfd2a2.chunk.css"
+  },
+  {
+    "revision": "58a6cc42172f7486ce2b",
+    "url": "/searchsquid/static/js/2.d85fcfd8.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/searchsquid/static/js/2.d85fcfd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89a85f79f77a3fafaa5c",
+    "url": "/searchsquid/static/js/main.7b07f237.chunk.js"
+  },
+  {
+    "revision": "20b7c01b6238f99a3413c7c13a1dab42",
+    "url": "/searchsquid/static/js/main.7b07f237.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24b5635fbf68ac3178bf",
+    "url": "/searchsquid/static/js/runtime-main.c03d3575.js"
+  }
+]);
